6dd2a3f06ce4a2838769c1f8e619803e8c06888c
Dillan <goberflunk@gmail.com>
2020-02-01 17:57:05 -0700

    Setting up react app and icon

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "YOUR_S3_UPLOADS_BUCKET_NAME"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://noczy7ux8f.execute-api.us-east-1.amazonaws.com/dev"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_yQZRZoaeW",
+    APP_CLIENT_ID: "6q8a1s30ic5ubnq2551d3st3p2",
+    IDENTITY_POOL_ID: "us-east-1:a8039103-d6d5-4bcb-8ac4-10d093a828d3"
+  }
+};
